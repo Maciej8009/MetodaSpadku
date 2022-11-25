93b6e6c2899e23ad199095a95e29eafcae1e1f82
Maciej8009 <maciejkowalik8009@gmail.com>
2022-11-25 23:44:46 +0100

    MetodaSpadku

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
     public static void main(String[] args) {
         double step=1.5;
-        double[] cordinates={1,5,0.3,7,0.5};
+        double[] cordinates={1,-3,0.5,-4,8};
         MetodaSpadku method=new MetodaSpadku(cordinates, step);
         method.steps();
         method.printX();
--- src/com/company/MetodaSpadku.java
+++ src/com/company/MetodaSpadku.java
@@ -9,7 +9,6 @@ public class MetodaSpadku {
     double vector;
     double f1;
     double f2;
-    double f3;
     double temp;
     double tempVector;
 
@@ -25,46 +24,35 @@ public class MetodaSpadku {
     }
     public void steps() {
         int licznik=0;
-        y[0]+=1;
-        while ((x0!=y)||(licznik<50)) {
+        while (licznik<1000) {
+            System.arraycopy(x0, 0, y, 0,x0.length);
             for (int i = 0; i < x0.length; i++) {
-//            System.out.println("przed vectorem");
-//            printX(x);
                 temp = x[i];
                 tempVector = vector;
                 System.arraycopy(x0, 0, y, 0, x0.length);
                 f1 = polymian(x0);
                 x[i] += vector;
                 f2 = polymian(x);
-//            System.out.println("po vectorze");
-//            printX(x);
                 if (f1 < f2) {
                     vector *= (-1);
                     x[i] = temp;
                     x[i] += vector;
                     f2 = polymian(x);
-//                System.out.println("Krok nie jest lepszy1");
-//                printX(x);
+
                     if (f1 < f2) {
                         vector = tempVector;
-//                    System.out.println("Krok nie jest lepszy2");
-//                    printX(x);
+
                     } else {
                         System.arraycopy(x, 0, x0, 0, x.length);
-//                    System.out.println("Krok jest lepszy1");
-//                    printX(x);
                     }
                 } else {
                     System.arraycopy(x, 0, x0, 0, x.length);
-//                System.out.println("Krok jest lepszy2");
-//                printX(x);
+
                 }
                 System.arraycopy(x0, 0, x, 0, x0.length);
                 vector = tempVector;
-                licznik++;
-//            System.out.println("X0 po iteracji pętli "+(int)(i+1));
-//            printX(x0);
             }
+            licznik++;
         }
     }
 
